287f2fed35a3352fe5aa15cbf65e139102a7888e
Rene Torres Alvarez <renetorres@Renes-MacBook-Air.local>
2021-02-26 23:25:53 -0600

    Using class components and multiple context

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Footer from './Footer';
 import Header from './Header';
 import Products from './Products';
 import { Routes, Route } from 'react-router-dom';
-import Detail from './Detail';
-
+//import Detail from './Detail';
+import Detail from './Detail.class';
 import Cart from './Cart';
-import Checkout from './Checkout';
+//import Checkout from './Checkout';
+import Checkout from './Checkout.class';
+
+import { useCart } from './cartContext';
 
 export default function App() {
+  const { dispatch } = useCart();
+
   return (
     <>
       <div className="content">
@@ -20,7 +25,10 @@ export default function App() {
             <Route path="/:category" element={<Products />} />
             <Route path="/:category/:id" element={<Detail />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
+            <Route
+              path="/checkout"
+              element={<Checkout dispatch={dispatch} />}
+            />
           </Routes>
         </main>
       </div>
